e4f0115d825ec5ea7bb2ba633210c8c1995ab130
jmsche <jonathan.scheiber@wikipower.be>
2024-02-06 13:06:52 +0100

    Allow test to fail

--- tests/Utility/UConfigurationTest.php
+++ tests/Utility/UConfigurationTest.php
@@ -7,26 +7,31 @@ namespace Leapt\FroalaEditorBundle\Tests\Utility;
 use Leapt\FroalaEditorBundle\Utility\UConfiguration;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\HttpClient;
 
 final class UConfigurationTest extends TestCase
 {
     public function testAllOptionsAreRegistered(): void
     {
-        $url = 'https://froala.com/wysiwyg-editor/docs/options/';
-        $httpClient = HttpClient::create();
-        $crawler = new Crawler($httpClient->request('GET', $url)->getContent());
-        $optionsFromDocs = $crawler->filter('.docs-data-list.block-box a');
-        $registeredOptions = UConfiguration::getArrOptionAll();
-        $proxyOrIgnoredOptions = ['Key', 'editor'];
-        $missingOptions = [];
+        try {
+            $url = 'https://froala.com/wysiwyg-editor/docs/options/';
+            $httpClient = HttpClient::create();
+            $crawler = new Crawler($httpClient->request('GET', $url)->getContent());
+            $optionsFromDocs = $crawler->filter('.docs-data-list.block-box a');
+            $registeredOptions = UConfiguration::getArrOptionAll();
+            $proxyOrIgnoredOptions = ['Key', 'editor'];
+            $missingOptions = [];
 
-        foreach ($optionsFromDocs as $option) {
-            if (!\in_array($option->textContent, $registeredOptions, true) && !\in_array($option->textContent, $proxyOrIgnoredOptions, true)) {
-                $missingOptions[] = $option->textContent;
+            foreach ($optionsFromDocs as $option) {
+                if (!\in_array($option->textContent, $registeredOptions, true) && !\in_array($option->textContent, $proxyOrIgnoredOptions, true)) {
+                    $missingOptions[] = $option->textContent;
+                }
             }
-        }
 
-        self::assertSame([], $missingOptions);
+            self::assertSame([], $missingOptions);
+        } catch (ClientException $e) {
+            $this->markTestSkipped(sprintf('Unable to retrieve options docs page: %s', $e->getMessage()));
+        }
     }
 }
